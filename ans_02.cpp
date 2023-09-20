bc7ff705341b83e574925214799da5cb9497da96
saurabhsingh108 <145419613+saurabhsingh108@users.noreply.github.com>
2023-09-20 08:17:30 +0530

    week1 answer's files are uplpded.
    these are the answers of Pwskills decode c++ with dsa batch of week 1. 
    All programs are compiled and executed in vscode,and the text based solutions are saved in .txt files.
    All program's screenshots are taken with output window .

---
+++ ans_02.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Sum of two positive numbers less than five is = " <<5+5 << endl;
+    return 0;
+
+}
---
+++ ans_05.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int rad=10;
+    float pi=3.14f;
+
+    float circmfrnce=2*pi*rad;
+
+    cout << circmfrnce << endl;
+
+    return 0;
+}
